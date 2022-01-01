abf9f5c6e951b07ff9421878cd4956a6b26243bc
Anish Jeyanth <Anish.Jeyanth@gds.ey.com>
2022-01-01 23:57:13 +0530

    Nullobject

---
+++ Pattern/NullObject.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Pattern
+{
+    public interface ICustomer
+    {
+        string Name { get; set; }
+        abstract String getName();
+    }
+
+    public class RealCustomer : ICustomer
+    {
+       public RealCustomer(string name)
+       {
+            Name = name;
+       }
+
+       public string Name { get ; set ; }
+
+       public string getName()
+       {
+            return Name;
+       }
+    }
+
+    public class NullCustomer : ICustomer
+    {
+
+        public string Name { get; set; } = "None";
+
+        public string getName()
+        {
+            return Name;
+        }
+    }
+
+    public class CustomerFactory
+    {
+        public static string[] names = {"Rob", "Joe", "Julie"};
+        public static ICustomer getCustomer(string name)
+        {
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i].Contains(name))
+                {
+                    return new RealCustomer(name);
+                }
+            }
+
+            return new NullCustomer();
+        }
+    }
+
+
+    internal class NullObject
+    {
+        public NullObject()
+        {
+            ICustomer customer1 = CustomerFactory.getCustomer("Rob");
+            ICustomer customer2 = CustomerFactory.getCustomer("Bob");
+            Console.WriteLine(customer1.getName());
+            Console.WriteLine(customer2.getName());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -26,6 +26,7 @@ public class Program
         //new States();
         //new Strategy();
         //new Template();
-        new Visitor();
+        //new Visitor();
+        new NullObject();
     }
 }
